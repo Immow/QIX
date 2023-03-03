a02946c68e83566dd331ddb7428e7d180ddd49f3
Immow <k.schippers@gmail.com>
2023-03-03 23:28:11 +0100

    bla

--- main.lua
+++ main.lua
@@ -1,21 +1,45 @@
-local function myStencilFunction()
-   -- Draw four overlapping circles as a stencil.
-   love.graphics.circle("fill", 200, 250, 100)
-   love.graphics.circle("fill", 300, 250, 100)
-   love.graphics.circle("fill", 250, 200, 100)
-   love.graphics.circle("fill", 250, 300, 100)
+local images = love.filesystem.getDirectoryItems("images")
+local index = 1
+local activeImage = love.graphics.newImage("images/"..images[index])
+local cW = 600
+local cH = 600
+local imageCanvas = love.graphics.newCanvas(cW, cH)
+local data = love.image.newImageData(cW,cH)
+local hide = nil
+function love.load()
+	for y = 1, 600-1 do
+		for x = 1, 600-1 do
+			data:setPixel(x, y, 0, 0, 0, 1)
+		end
+	end
+
+	for y = 1, 100 do
+		for x = 1, 100 do
+			data:setPixel(x, y, 1, 1, 1, 0)
+		end
+	end
+
+	hide = love.graphics.newImage(data)
+	love.graphics.setCanvas(imageCanvas)
+	local w = imageCanvas:getWidth()
+	local h = imageCanvas:getHeight()
+	local imgW = activeImage:getWidth()
+	local imgH = activeImage:getHeight()
+	local scale = 1
+	if imgW > imgH then
+		scale = w / imgW
+	else
+		scale = h / imgH
+	end
+	love.graphics.draw(activeImage, 0, 0, 0, scale, scale)
+	love.graphics.setCanvas()
+
 end
- 
+
+function love.update() end
+
 function love.draw()
-   -- Each pixel touched by each circle will have its stencil value incremented by 1.
-   -- The stencil values for pixels where the circles overlap will be at least 2.
-   love.graphics.stencil(myStencilFunction, "increment")
- 
-   -- Only allow drawing in areas which have stencil values that are less than 2.
-   love.graphics.setStencilTest("less", 2)
- 
-   -- Draw a big rectangle.
-   love.graphics.rectangle("fill", 0, 0, 500, 500)
- 
-   love.graphics.setStencilTest()
+	love.graphics.draw(imageCanvas, 0,0)
+	love.graphics.rectangle("line", 0, 0, cW ,cH)
+	love.graphics.draw(hide)
 end
